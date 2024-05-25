6f5d9d9847cea756a289347b6f91cabf1a1ae6f9
long-micro <2070174292@qq.com>
2024-05-25 13:47:05 +0800

    init

---
+++ UDP_client.c
@@ -0,0 +1,60 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+static int client_init(const char *ip_addr, const char *ip_port, struct sockaddr_in *addr_server)
+{
+    int sockfd = 0;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        perror("sockfd error");
+        return -1;
+    }
+
+    addr_server->sin_family = AF_INET;
+    addr_server->sin_addr.s_addr = inet_addr(ip_addr);
+    addr_server->sin_port = htons(atoi(ip_port));
+
+    return sockfd;
+}
+
+static void send_msg(int sockfd, struct sockaddr_in addr_server)
+{
+    while (1) {
+        char buff[1024] = {0};
+
+        (void)fgets(buff, sizeof(buff), stdin);
+        buff[strlen(buff) - 1] = '\0';
+
+        int sended = sendto(sockfd, buff, sizeof(buff), 0, (struct sockaddr *)&addr_server, sizeof(addr_server));
+        if (sended < 0) {
+            perror("sendto error");
+            continue;
+        }
+
+        int quited = strncmp(buff, "quit", 4);
+        if (0 == quited) {
+            printf("client quit\n");
+            break;
+        }
+    }    
+}
+
+int main (int argv, char *argc[])
+{
+    int sockfd = 0;
+    struct sockaddr_in addr_server = {0};
+
+    sockfd = client_init("172.31.159.18", "7777", &addr_server);
+
+    send_msg(sockfd, addr_server);
+
+    close(sockfd);
+
+    return 0;
+}
---
+++ UDP_server.c
@@ -0,0 +1,66 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+
+static int server_init(const char *ip_addr, const char *ip_port)
+{   
+    int sockfd = 0;
+    struct sockaddr_in addr_server = {0};
+
+    addr_server.sin_family = AF_INET;
+    addr_server.sin_addr.s_addr = inet_addr(ip_addr);
+    addr_server.sin_port = htons(atoi(ip_port));
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        perror("socket error");
+        return -1;
+    }
+
+    socklen_t addr_len = sizeof(struct sockaddr_in);
+    int binded = bind(sockfd, (struct sockaddr *)&addr_server, addr_len);
+    if (binded < 0) {
+        close(sockfd);
+        perror("bind error");
+        return -1;
+    }
+
+    return sockfd;
+}
+
+static void recv_msg(int sockfd)
+{
+    while (1) {
+        char buff[1024] = {0};
+
+        size_t bytes = recvfrom(sockfd, buff, sizeof(buff), 0, NULL, NULL);
+        if (bytes < 0) {
+            perror("recvfrom error");
+            continue;
+        }
+        else {
+            int quited = strcmp(buff, "quit");
+            if (0 == quited){
+                printf("sever quit\n");
+                break;
+            }
+            else{
+                printf("%s\n", buff);
+            }
+        }
+    }
+}
+
+int main (int agv, char *argc[])
+{
+    int sockfd = server_init("172.31.159.18", "7777");
+    recv_msg(sockfd);
+
+    close(sockfd);
+    return 0;
+}
