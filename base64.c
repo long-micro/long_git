10101583e7e5a4ebc35c70a66e4ac144c14ba057
long-micro <2070174292@qq.com>
2024-05-31 18:34:48 +0800

    修改半成品

--- base64.c
+++ base64.c
@@ -4,6 +4,12 @@
 
 #define padding '='
 
+struct three_bytes {
+    uint8_t byte1;
+    uint8_t byte2;
+    uint8_t byte3;
+};
+
 static const char base64_table[64] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 static int char_to_base64(char c)
@@ -16,40 +22,79 @@ static int char_to_base64(char c)
     return -1;
 }
 
-static int base64_encode(const char *unencrypted, const unsigned int bytes, char *encrypted)
-{   
-    int i,j;
-    for (i = 0, j = 0; i < bytes; i+=3) {
-        
-        uint8_t byte1 = i < bytes ? unencrypted[i] : 0;
-        uint8_t byte2 = (i+1) < bytes ? unencrypted[i+1] : 0;
-        uint8_t byte3 = (i+2) < bytes ? unencrypted[i+2] : 0;
+static uint8_t *generate_base64(struct three_bytes three_bytes)
+{
+    static uint8_t base64[4]; 
 
-        encrypted[j++] = base64_table[byte1 >> 2];
-        encrypted[j++] = base64_table[((byte1 & 0x03) << 4) + (byte2 >> 4)];
-        encrypted[j++] = base64_table[((byte2 & 0x0F) << 2) + (byte3 >> 6)];
-        encrypted[j++] = base64_table[byte3 & 0x3F];
-    }
+    base64[0] = three_bytes.byte1 >> 2;
+    base64[1] = ((three_bytes.byte1 & 0x03) << 4) + (three_bytes.byte2 >> 4);
+    base64[2] = ((three_bytes.byte2 & 0x0F) << 2) + (three_bytes.byte3 >> 6);
+    base64[3] = three_bytes.byte3 & 0x3F;
 
+    return base64;
+}
+
+static uint8_t *generate_ascii(uint8_t *base64)
+{
+    static uint8_t ascii[3];
+
+    ascii[3] = (base64[0] << 2) + (base64[1] >> 4);
+    ascii[3] = (base64[1] << 4) + (base64[2] >> 2);
+    ascii[3] = (base64[2] << 6) + (base64[3]);
+
+    return ascii;
+}
+
+static void padding_append(const unsigned int bytes, char *encrypted, int index)
+{
     if(2 == (bytes %3)) {
-        encrypted[j-1] = padding;
+        encrypted[index-1] = padding;
     }
 
     if(1 == (bytes %3)) {
-        encrypted[j-1] = padding;
-        encrypted[j-2] = padding;
+        encrypted[index-1] = padding;
+        encrypted[index-2] = padding;
+    }
+}
+
+static void padding_remove(int padding_num, char *unencrypted, int index)
+{
+    if (2 == padding_num) {
+        unencrypted[index - 2] = '\0';
     }
 
-    encrypted[j] = '\0';
-    
-    return 0;
+    if (1 == padding_num) {
+        unencrypted[index - 1] = '\0';
+    }
+}
+
+static void base64_encode(const char *unencrypted, const unsigned int bytes, char *encrypted)
+{   
+    int index = 0;
+    struct three_bytes three_bytes = {0};
+
+    for (int i = 0; i < bytes; i+=3) {
+        three_bytes.byte1 = i < bytes ? unencrypted[i] : 0;
+        three_bytes.byte2 = (i+1) < bytes ? unencrypted[i+1] : 0;
+        three_bytes.byte3 = (i+2) < bytes ? unencrypted[i+2] : 0;
+
+        uint8_t *base64 = generate_base64(three_bytes);
+        encrypted[index++] = base64_table[base64[0]];
+        encrypted[index++] = base64_table[base64[1]];
+        encrypted[index++] = base64_table[base64[2]];
+        encrypted[index++] = base64_table[base64[3]];
+    }
+
+    padding_append(bytes, encrypted, index);
+
+    encrypted[index] = '\0';
 }
 
 static int base64_decode(const char *encrypted, const unsigned int bytes, char *unencrypted)
 {
-    uint8_t base64_num[4] = {0};
-    uint8_t asccii_num[3] = {0};
+    int index = 0;
     int padding_num = 0;
+    uint8_t base64[4] = {0};
 
     if ('=' == encrypted[bytes-2]) {
         padding_num = 2;
@@ -58,50 +103,38 @@ static int base64_decode(const char *encrypted, const unsigned int bytes, char *
         padding_num = 1;
     }
 
-    int i = 0, j = 0;
-    for (i = 0; i < bytes; i+=4) {
-        for (int k = 0; k < 4 && (i + k) < bytes - padding_num; k++) {
-            base64_num[k] = char_to_base64(encrypted[i+k]);
-            if (-1 == base64_num[k]) {
+    for (int i = 0; i < bytes; i+=4) {
+        for (int k = 0; k < 4 && (i + k) < (bytes - padding_num); k++) {
+            base64[k] = char_to_base64(encrypted[i+k]);
+            if (-1 == base64[k]) {
                 printf("非法字符\n");
                 return -1;
             }
         }
 
-        asccii_num[0] = (base64_num[0] << 2) + (base64_num[1] >> 4);
-        asccii_num[1] = (base64_num[1] << 4) + (base64_num[2] >> 2);
-        asccii_num[2] = (base64_num[2] << 6) + (base64_num[3]);
-
-        unencrypted[j++] = asccii_num[0];
-        unencrypted[j++] = asccii_num[1];
-        unencrypted[j++] = asccii_num[2];
+        uint8_t *ascii = generate_ascii(base64);
+        unencrypted[index++] = (base64[0] << 2) + (base64[1] >> 4);
+        unencrypted[index++] = (base64[1] << 4) + (base64[2] >> 2);
+        unencrypted[index++] = (base64[2] << 6) + (base64[3]);
     }
 
-    if (2 == padding_num) {
-        unencrypted[j - 2] = '\0';
-    }
+    padding_remove(padding_num, unencrypted, index);
 
-    if (1 == padding_num) {
-        unencrypted[j - 1] = '\0';
-    }
-
-    unencrypted[j] = '\0';
+    unencrypted[index] = '\0';
 
     return 0;
 }
 
 int main(int argc, char *argv[])
 {
-    char original[] = "hello, world!";
-    char encrypted[100] ={0};
-    char unencrypted[100] ={0};
+    char original[1024] = "hello1, world!";
+    char encrypted[1024] ={0};
+    char unencrypted[1024] ={0};
 
     base64_encode(original, strlen(original), encrypted);
-
     printf("%s\n", encrypted);
 
     base64_decode(encrypted, strlen(encrypted), unencrypted);
-
     printf("%s\n", unencrypted);
 
     return 0;
